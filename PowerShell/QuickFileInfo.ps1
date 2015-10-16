210eda05a83bc15c487f95e8fd7d741e77cd8e88
etokrug <etokrug@gmail.com>
2015-10-15 19:28:34 -0700

    file info script port

---
+++ PowerShell/QuickFileInfo.ps1
@@ -0,0 +1,12 @@
+﻿$oPath = "$($pwd)\OutFile.txt"
+if (Test-Path $oPath) {
+    Remove-Item $oPath;
+}
+
+$files = Get-ChildItem -Recurse $fPath
+
+foreach ($file in $files) {
+    if (!($file -is [System.IO.DirectoryInfo])) {
+        "$($file.FullName): $($file.Length / 1024 /1024) MB" >> $oPath;
+    }
+}
